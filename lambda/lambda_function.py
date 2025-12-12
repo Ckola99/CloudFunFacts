88aa4be400be8b12b9bc9280615a5a7c0e8d9d29
Christopher kola <Christopherkola@gmail.com>
2025-12-12 14:37:23 +0200

    finished readme

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -89,6 +89,9 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "headers": {
             "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type"
         },
         "body": json.dumps({"fact": witty_fact})
     }
